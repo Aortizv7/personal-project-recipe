8de7e1de9abc14710c14730814712306fcbfb69f
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-11-08 16:07:59 -0700

    added animation to home text

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.css';
+import Typist from 'react-typist';
 
-export default function Home() {
-    return (
-        <div className='home_background'>
-            <div>
-                <header className='home_header'>Cooking with Love,
-                    Provides food for the Soul!</header>
+export default class Home extends Component {
+    render() {
+        return (
+            <div className='home_background'>
+                <div>
+                    <Typist>
+                        <header className='home_header'> Cooking with Love, Provides food for the Soul!</header>
+                    </Typist>
+                </div>
+                <div>
+                    <a href={process.env.REACT_APP_LOGIN}>
+                        <button className='home_button'>Feed my Soul!</button>
+                    </a>
+                </div>
             </div>
-            <div>
-                <a href={process.env.REACT_APP_LOGIN}>
-                    <button className='home_button'>Feed my Soul!</button>
-                </a>
-            </div>
-        </div>
-    )
+        )
+    }
 }
