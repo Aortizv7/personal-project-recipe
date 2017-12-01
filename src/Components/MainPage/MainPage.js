31de44714f68122e7ab958339506941a5f2b376b
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-11-30 17:37:56 -0700

    minor style changes

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -54,16 +54,20 @@ export default class NavBar extends Component {
         return (
             <main className='mainPage_background'>
                 <nav className='mainPage_navBackground'>
-                    <div className='fork_and_spoon_logo'></div>
-                    <div className='mainPage_title'>Recipe Jar</div>
-                    <input type='text'
-                        placeholder='Search by Ingredients/Recipe Name'
-                        value={this.state.userInput}
-                        className='input_box'
-                        onChange={(e) => this.handleChange(e)}
-                    />
-                    <button className='search_button'
-                        onClick={this.handleClick}>Search</button>
+                    <div className='logo_position'>
+                        <div className='fork_and_spoon_logo'></div>
+                        <div className='mainPage_title'>Recipe Jar</div>
+                    </div>
+                    <div>
+                        <input type='text'
+                            placeholder='Search by Ingredients/Recipe Name'
+                            value={this.state.userInput}
+                            className='input_box'
+                            onChange={(e) => this.handleChange(e)}
+                        />
+                        <button className='search_button'
+                            onClick={this.handleClick}>Search</button>
+                    </div>
                     <section className='mainPage_menu'>
                         <div className='mainPage_menu_options'>
                             <Link to='/profile'>
@@ -89,9 +93,9 @@ export default class NavBar extends Component {
 function RecipeCard(props) {
     return (
         <div className='mainPage_recipe_info'>
-           
-                <h2 className='title'>{props.title}</h2>
-            
+
+            <h2 className='title'>{props.title}</h2>
+
             <Link to={`/recipe/detail/${props.recipe_id}`}>
                 <img className='mainPage_recipe_img'
                     src={props.image_url}
